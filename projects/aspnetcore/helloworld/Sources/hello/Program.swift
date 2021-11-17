27e2a3f4ee8b6f0d91aa9167eb6e07b9bcba8302
Eric Sink <eric@sourcegear.com>
2021-11-17 10:28:14 -0600

    update to sgbridge 0.3.1.  tighten up the aspnetcore/helloworld sample.

--- projects/aspnetcore/helloworld/Sources/hello/Program.swift
+++ projects/aspnetcore/helloworld/Sources/hello/Program.swift
@@ -1,60 +1,35 @@
 
-import aspnetcore; 
 import dotnet; 
+import aspnetcore; 
 
-typealias Action = System.Action_1;
+public func swift_main() throws -> Swift.Int32 {
+    try Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()!
+        .ConfigureWebHostDefaults
+        {
+            webHostBuilder in
 
-@_cdecl("swift_main")
-public func swift_main() -> Swift.Int32 
-{
-
-    do 
-    {
-        let f_mapRoutes = 
-            try Action<Microsoft.AspNetCore.Routing.IEndpointRouteBuilder>
-            {
-                routes in
-
-                try routes.MapGet(pattern: "/")
+            try webHostBuilder
+                .Configure
                 {
-                    context in
+                    app in
 
-                    return try await context.Response.WriteAsync(text: "Hello World");
-                };
-            }
+                    try app
+                        .UseRouting()
+                        .UseEndpoints
+                        {
+                            routes in
 
-        try Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()!
-            .ConfigureWebHostDefaults
-            {
-                webHostBuilder in
+                            try routes.MapGet(pattern: "/")
+                            {
+                                context in
 
-                try webHostBuilder
-                    .Configure
-                    {
-                        app in
-
-                        try app
-                            .UseRouting()
-                            .UseEndpoints(configure: f_mapRoutes)
-                            ;
-                    }
-                    ;
-            }
-            .Build()!
-            .Run()
-            ;
-        return 0;
-    }
-    catch let e as System.Exception 
-    {
-        try! System.Console.WriteLine(value: e.ToString());
-        return -1;
-    }
-    catch
-    {
-        print("Unhandled error of some kind");
-        return -1;
-    }
-
+                                try await context.Response.WriteAsync(text: "Hello World");
+                            }
+                        }
+                }
+        }
+        .Build()!
+        .Run()
+    return 0;
 }
 
